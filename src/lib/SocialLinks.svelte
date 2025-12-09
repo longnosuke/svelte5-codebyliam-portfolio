753f41da155ca206a34e9e8e9d49b1f5da31e145
Thanh Long <thanhlongnosuke@gmail.com>
2025-12-09 20:26:00 +0700

    feat(social-links): add SocialLinks component
    Introduce SocialLinks.svelte to display GitHub, LinkedIn, and email icons with Font Awesome. Provides a reusable UI element for social contact links.

---
+++ src/lib/SocialLinks.svelte
@@ -0,0 +1,65 @@
+<div class="container">
+    <div class="row">
+        <div class="col-md-12 text-center">
+            <ul class="list-inline social-links">
+
+                <!-- GitHub -->
+                <li>
+                    <a href="https://github.com/#" class="btn-social" title="GitHub">
+                        <i class="fa-brands fa-github"></i>
+                    </a>
+                </li>
+
+                <!-- LinkedIn -->
+                <li>
+                    <a href="https://ph.linkedin.com/in/#" class="btn-social" title="LinkedIn">
+                        <i class="fa-brands fa-linkedin"></i>
+                    </a>
+                </li>
+
+                <!-- Email -->
+                <li>
+                    <a href="mailto:youremail@example.com" class="btn-social" title="Email">
+                        <i class="fa-solid fa-envelope"></i>
+                    </a>
+                </li>
+
+            </ul>
+        </div>
+    </div>
+</div>
+
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
+
+<style>
+  .btn-social {
+    display: inline-block;
+    width: 48px;
+    height: 48px;
+    border-radius: 50%;
+    text-align: center;
+    font-size: 24px;
+    line-height: 48px;
+    color: white;
+    transition: 0.3s ease;
+  }
+
+  .list-inline {
+    padding-left: 0;
+    list-style: none;
+    margin-left: -5px;
+    margin-bottom: 0;
+    margin-top: 0;
+  }
+  .list-inline > li {
+    display: inline-block;
+    padding-left: 5px;
+    padding-right: 5px;
+  }
+
+  .social-links {
+    animation: fade-in-down 0.9s ease;
+    animation-delay: 0.2s;
+    display: flex;
+  }
+</style>
